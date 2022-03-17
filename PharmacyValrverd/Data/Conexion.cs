1d22dca79d37a0ca12844a29c38a5a0cae9eae2b
yana1996 <yana052196@gmail.com>
2022-03-17 00:05:21 -0600

    Agregar archivos de proyecto.

---
+++ PharmacyValrverd/Data/Conexion.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+
+namespace PharmacyValrverd.Data
+{
+    public class Conexion
+    {
+
+        private string conexion;
+
+        public Conexion(IConfiguration configuration)
+        {
+            conexion = configuration.GetConnectionString("defaultConnection");
+
+        }
+
+        public void prueba()
+        {
+
+            using (SqlConnection sql = new SqlConnection(conexion))
+            {
+
+                sql.Open();
+                SqlCommand command = new SqlCommand("SELECT * From Prueba;", sql);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(1));
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+    }
+
+    
+}
