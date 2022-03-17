280368b039cf9e51ce731f2b5929b790ee8986ef
kalet.j.myrie.soto <kalo24694@gmail.com>
2022-03-17 14:09:25 -0600

    Create Login

--- PharmacyValrverd/Startup.cs
+++ PharmacyValrverd/Startup.cs
@@ -52,7 +52,7 @@ namespace PharmacyValrverd
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
